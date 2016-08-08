e34c2f490bf12794c59bf4ed697227d1e760e3f0
Thinh Luong <tluong@pillartechnology.com>
2016-08-08 14:38:43 -0500

    Add sample code to run pep8radius diff

--- example.py
+++ example.py
@@ -21,3 +21,16 @@ class Example3(   object ):
 Only actual code should be reindented.
 """
                     return (sys.path, some_string)
+
+
+def new_sample_code(): return {'has_key() is deprecated':True}.has_key({'f':2}.has_key(''));
+class SomeClass(   object ):
+    def __init__    ( self, bar ):
+     #Comments should have a space after the hash.
+     if bar : bar+=1;  bar=bar* bar   ; return bar
+     else:
+                    some_string = """
+               Indentation in multiline strings should not be touched.
+Only actual code should be reindented.
+"""
+                    return (sys.path, some_string)
